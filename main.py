c3af9ad4e358c0dc3b600a87035b266d1a6e69c1
Matthew Jiang <jiangmy@usc.edu>
2023-10-23 14:43:46 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from task import task
+import numpy as np
+import matplotlib.pyplot as plt
+
+tasks = []
+
+def phi(x, x0):
+    return np.linalg.norm(x-x0)
+
+def grad(x, x0):
+    return x-x0
+
+def get_grad_sum(x, tasks):
+    res = np.zeros(len(x))
+    for t in tasks:
+        res += t.get_grad(x)
+    return res
+    
+
+if __name__ == "__main__":
+    
+    # create 5 tasks with random coordinates and equal value gain
+    d = 2
+    
+    for i in range(5):
+        x = np.random.randint(0, 50, d)
+        tasks.append(task(x, np.random.randint(0, 10), phi, grad))
+    
+        
+    # plot the tasks
+    
+    # 50 x 50 linspace matrix
+    
+    x, y = np.meshgrid(
+    np.arange(0, 50, 50),
+    np.arange(0, 50, 50),
+    sparse=False)
+    
+    
+    
+    fig, ax = plt.subplots()
+    ax.set_xlim(0, 50)
+    ax.set_ylim(0, 50)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title('Tasks')
+    ax.grid(True)
+    
+    points = [[x, y] for x in range(0, 50, 1) for y in range(0, 50, 1)]
+    vals = [tasks[0].get_grad(p) for p in points]
+    ax.quiver([p[0] for p in points], [p[1] for p in points], [v[0] for v in vals], [v[1] for v in vals])
+    ax.plot([t.x[0] for t in tasks], [t.x[1] for t in tasks], 'ro')
+      
+    plt.show()
+        
+    
---
+++ task.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class task():
+    # x: coordinates x in R^d
+    # v_star: value gain
+    # phi: value function phi in R^d
+    def __init__(self, x, v_star, phi, grad):
+        self.x = x
+        self.v_star = v_star
+        self.phi = phi
+        self.grad = grad
+        
+    def get_phi(self, x):
+        return self.phi(self.x, x)
+    
+    def get_grad(self, x):
+        return self.grad(self.x, x)
+
+    
